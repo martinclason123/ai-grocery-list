f997470b045e818bcf4a83f5a128d26cfe93a319
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-03-18 09:24:30 -0400

    question 7

--- components/Question7.js
+++ components/Question7.js
@@ -1,18 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import { QuestionContainer, QuestionHeading, NextButton } from "./sharedStyles";
 
 const days = [1, 2, 3, 4, 5, 6, 7];
 
-const Question7 = ({ value, onChange }) => {
+const RadioContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const StyledRadioButton = styled.input`
+  margin-right: 8px;
+`;
+
+const Question7 = ({ value, onChange, onNext, onBack }) => {
   const handleChange = (e) => {
     onChange(parseInt(e.target.value, 10));
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowRight") {
+      onNext();
+    } else if (e.key === "ArrowLeft") {
+      onBack();
+    }
+  };
+
   return (
-    <div>
-      <h3>How many days should this meal plan cover?</h3>
+    <QuestionContainer onKeyDown={handleKeyDown}>
+      <QuestionHeading>
+        How many days should this meal plan cover?
+      </QuestionHeading>
       {days.map((day, index) => (
-        <div key={index}>
-          <input
+        <RadioContainer key={index}>
+          <StyledRadioButton
             type="radio"
             id={`day-${index}`}
             name="days"
@@ -23,9 +45,10 @@ const Question7 = ({ value, onChange }) => {
           <label htmlFor={`day-${index}`}>
             {day} day{day > 1 && "s"}
           </label>
-        </div>
+        </RadioContainer>
       ))}
-    </div>
+      <NextButton onClick={onNext}>Next</NextButton>
+    </QuestionContainer>
   );
 };
 
