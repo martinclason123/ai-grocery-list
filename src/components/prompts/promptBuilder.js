e2d8e88ac3cd554a989e925cda55cd4c477ad19b
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-03-21 13:39:34 -0400

    changes

--- src/components/prompts/promptBuilder.js
+++ src/components/prompts/promptBuilder.js
@@ -1,15 +1,19 @@
 export const buildPrompt = (formData) => {
-  const prompt = `Provide me a meal plan with titles only for ${
+  const prompt = `Provide me a meal plan with titles for exactly ${
     formData.days
   } days, that will feed ${formData.adults} adults and ${
     formData.children
-  } kids. The meals should be available for purchase at ${
+  } kids. Only choose meals with ingredients that have a high chance of being available for purchase at ${
     formData.store
-  } grocery store, and should prioritize ${formData.priorities.join(
+  } grocery store, and should prioritize the following: ${formData.priorities.join(
     ", "
   )}. Make sure none of the ingredients contain ${formData.restrictions.join(
     ", "
-  )}. Organize the plan by day, but only include titles at this time. The format should be as follows:
+  )}. The only meals that should be included are as follows: ${formData.meals.join(
+    ", "
+  )}.
+  
+  Organize the plan by day, but only include titles at this time. The format should be as follows:
   
     Day 1
     Dinner: [generated response]
