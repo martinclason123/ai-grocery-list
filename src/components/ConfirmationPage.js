8daa113f74057364afb8643d73a41c1efe4da03e
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-03-22 06:34:41 -0400

    working again

--- src/components/ConfirmationPage.js
+++ src/components/ConfirmationPage.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { replaceMeal, fetchMeals } from "./mealFunctions";
 import {
   ConfirmationContainer,
   MealList,
   Meal,
   BackButton,
-  LoadingText,
-  ErrorText,
-  ReplaceButton,
-  MealContainer,
-  ButtonsContainer,
 } from "@/styles/ConfirmationPageStyles";
 
 const ConfirmationPage = ({ formData, onBackButtonClick }) => {
@@ -18,100 +12,52 @@ const ConfirmationPage = ({ formData, onBackButtonClick }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const storedMeals = sessionStorage.getItem("meals");
-    if (storedMeals) {
-      setMeals(JSON.parse(storedMeals));
-    } else {
-      fetchMeals(formData, setIsLoading, setError, setMeals);
-    }
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("/api/fetch_meals", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch meals.");
+        }
+
+        const data = await response.json();
+        setMeals(data.meals);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchMeals();
   }, []);
 
-  // const fetchMeals = async () => {
-  //   setIsLoading(true);
-  //   setError(null);
-
-  //   try {
-  //     const response = await fetch("/api/fetch_meals", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(formData),
-  //     });
-
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       setMeals(data.mealPlan);
-  //       sessionStorage.setItem("meals", JSON.stringify(data.mealPlan));
-  //     } else {
-  //       const errorData = await response.json();
-  //       setError(
-  //         `Error: ${
-  //           errorData.message ||
-  //           "An error occurred while fetching meals. Please try again."
-  //         }`
-  //       );
-  //     }
-  //   } catch (error) {
-  //     setError(
-  //       `Error: ${
-  //         error.message ||
-  //         "An error occurred while fetching meals. Please try again."
-  //       }`
-  //     );
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
+  const mealsToDisplay = meals.slice(0, parseInt(formData.days));
 
   return (
     <ConfirmationContainer>
       <h1>Meal Plan</h1>
       {isLoading ? (
-        <LoadingText>Loading...</LoadingText>
+        <p>Loading...</p>
       ) : error ? (
-        <ErrorText>{error}</ErrorText>
+        <p>{error}</p>
       ) : (
         <MealList>
-          {meals.map((day, index) => {
-            const dayKey = Object.keys(day)[0];
-            const dayMeals = day[dayKey];
-            return (
-              <div key={index}>
-                <h3>{dayKey}</h3>
-                {dayMeals.map((meal, mealIndex) => {
-                  const mealType = Object.keys(meal)[0];
-                  const mealTitle = meal[mealType];
-                  return (
-                    <Meal key={mealIndex}>
-                      <MealContainer>
-                        <p>
-                          {mealType}: {mealTitle}
-                        </p>
-                        <ReplaceButton
-                          onClick={() => {
-                            replaceMeal("Meal to replace:", mealTitle);
-                          }}
-                        >
-                          Replace
-                        </ReplaceButton>
-                      </MealContainer>
-                    </Meal>
-                  );
-                })}
-              </div>
-            );
-          })}
+          {mealsToDisplay.map((meal, index) => (
+            <Meal key={index}>
+              <p>
+                Day {index + 1}: {meal}
+              </p>
+            </Meal>
+          ))}
         </MealList>
       )}
-      <BackButton
-        onClick={() => {
-          sessionStorage.removeItem("meals");
-          onBackButtonClick();
-        }}
-      >
-        Back
-      </BackButton>
+      <BackButton onClick={onBackButtonClick}>Back</BackButton>
     </ConfirmationContainer>
   );
 };
--- src/components/prompts/promptBuilder.js
+++ src/components/prompts/promptBuilder.js
@@ -1,39 +1,27 @@
 export const buildPrompt = (formData) => {
-  const mealTypes = formData.meals
-    .map((mealType) => {
-      return `{ "${mealType}": "Example ${mealType}" }`;
-    })
-    .join(", ");
-
-  const dayExamples = Array(formData.days)
-    .fill()
-    .map((_, index) => {
-      return `{
-        "day${index + 1}": [
-          ${mealTypes}
-        ]
-      }`;
-    })
-    .join(", ");
-
-  const prompt = `Create a meal plan for exactly ${
+  const totalMeals = parseInt(formData.days, 10) + 5;
+  const prompt = `Create a meal plan for ${
     formData.days
   } days, that will feed ${formData.adults} adults and ${
     formData.children
-  } children. Only choose meals with ingredients that have a high chance of being available for purchase at ${
+  } children. Include an additional 5 extra dinner meal options, for a total of ${totalMeals} dinner meals. Only choose meals with ingredients that have a high chance of being available for purchase at ${
     formData.store
   } grocery store, and should prioritize the following aspects: ${formData.priorities.join(
     ", "
   )}. Make sure none of the ingredients contain ${formData.restrictions.join(
     ", "
-  )}. The only meals that should be included are: ${formData.meals.join(", ")}.
-    
-    Please provide the meal plan in the following JSON format:
-    {
-      "mealPlan": [
-        ${dayExamples}
-      ]
-    }`;
+  )}. The only meal that should be included is dinner.
+
+Please provide the meal plan in the following JSON format:
+{
+  "meals": [
+    "Meal 1",
+    "Meal 2",
+    "Meal 3",
+    ...
+    "Meal ${totalMeals}"
+  ]
+}`;
 
   return prompt;
 };
