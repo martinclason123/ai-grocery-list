79ac858dc5626f6cd7e512008367497b831ab7c6
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-03-25 15:01:33 -0400

    great success

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,3 +1,5 @@
+import { react, useState } from "react";
+
 import {
   ShoppingListContainer,
   GroceryList,
@@ -9,7 +11,8 @@ import { shoppingListPrompt } from "./prompts";
 import { getGroceryList } from "./mealFunctions";
 
 const ShoppingList = ({ listData, formData }) => {
-  const handleGetShoppingList = (data) => {
+  const [list, setList] = useState(null);
+  const handleGetShoppingList = async (data) => {
     const titles = data.map((item) => item.title).join(", ");
     const ingredients = data
       .map((item) => item.recipe)
@@ -30,14 +33,20 @@ const ShoppingList = ({ listData, formData }) => {
         in the items on this shopping list.
         `;
     }
-    // const allergies = `${formData.restrictions.join(", ")}`;
 
     console.log(allergies);
     const store = formData.store;
     console.log("preferred store:", store);
 
     const prompt = shoppingListPrompt(allergies, store, ingredients, titles);
-    getGroceryList(prompt);
+    const groceryList = await getGroceryList(prompt);
+    /* grocery list looks like this if logged:
+       {text: '\n\nAldi Shopping List:\n\nProduce:\n2 red bell peppers…e\nRice vinegar\nHoney\n\nBakery:\n4-6 flour tortillas', index: 0, logprobs: null, finish_reason: 'stop'}
+
+      I would like the data reformatted so it can be rendered to the user as shown in return. 
+    */
+    // setList(groceryList.text);
+    console.log(groceryList);
   };
   return (
     <ShoppingListContainer>
@@ -48,6 +57,25 @@ const ShoppingList = ({ listData, formData }) => {
       >
         Get Shopping List
       </GetShoppingList>
+      <div>
+        {list}
+        {/* 
+        <h1>Aldi Shopping List</h1>
+         <div>
+           <h2>Produce</h2>
+           <ul>
+            <li>2 red bell pepper</li>
+           </ul>
+        </div> 
+         <div>
+           <h2>Meat</h2>
+           <ul>
+            <li>2 boneless skinless chicken breasts</li>
+           </ul>
+           ...etc
+        </div> 
+        */}
+      </div>
     </ShoppingListContainer>
   );
 };
--- src/components/mealFunctions/getGroceryList.js
+++ src/components/mealFunctions/getGroceryList.js
@@ -12,6 +12,31 @@ const getGroceryList = async (prompt) => {
 
     const data = await response.json();
     console.log(data.choices[0]);
+    /* data.choices[0] looks like this if logged:
+       {text: '\n\nAldi Shopping List:\n\nProduce:\n2 red bell peppers…e\nRice vinegar\nHoney\n\nBakery:\n4-6 flour tortillas', index: 0, logprobs: null, finish_reason: 'stop'}
+
+      I would like the data reformatted so it can be returned as an array of objects instead. for example
+      [{department: "Produce", list: [carrots, peas]}, {department: "Meat", list: ["2 boneless skinless chicken breasts"]}] 
+
+      The title "Aldi Shopping List" can be discarded
+    */
+    const lines = data.choices[0].text
+      .split("\n")
+      .filter((line) => line.trim() !== "");
+    const formattedList = [];
+
+    for (const line of lines) {
+      if (line.includes("Shopping List")) {
+        continue;
+      } else if (line.endsWith(":")) {
+        formattedList.push({ department: line.slice(0, -1), list: [] });
+      } else {
+        formattedList[formattedList.length - 1].list.push(line);
+      }
+    }
+
+    console.log(formattedList);
+    return formattedList;
   } catch (error) {
     console.log(error);
   }
