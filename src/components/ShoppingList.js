b9eb958468e00b7e36197b5042d2d9b309b9b349
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-03-27 15:38:41 -0400

    shopping list progress

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -12,51 +12,79 @@ import { getGroceryList } from "./mealFunctions";
 
 const ShoppingList = ({ listData, formData }) => {
   const [list, setList] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const handleGetShoppingList = async (data) => {
-    const titles = data.map((item) => item.title).join(", ");
-    const ingredients = data
-      .map((item) => item.recipe)
-      .flat()
-      .join(", ");
+    try {
+      setLoading(true);
+      const titles = data.map((item) => item.title).join(", ");
+      const ingredients = data
+        .map((item) => item.recipe)
+        .flat()
+        .join(", ");
 
-    console.log("Titles:", titles);
-    console.log("Ingredients:", ingredients);
-    console.log(typeof formData);
-    console.log(formData);
-    let allergies;
+      console.log("Titles:", titles);
+      console.log("Ingredients:", ingredients);
+      console.log(typeof formData);
+      console.log(formData);
+      let allergies;
 
-    const allergiesList = formData.restrictions.join(", ");
-    if (allergiesList === "") {
-      allergies = "";
-    } else {
-      allergies = `I have severe allergies to the following: ${allergiesList}, ensure they are not included
+      const allergiesList = formData.restrictions.join(", ");
+      if (allergiesList === "") {
+        allergies = "";
+      } else {
+        allergies = `I have severe allergies to the following: ${allergiesList}, ensure they are not included
         in the items on this shopping list.
         `;
-    }
+      }
 
-    console.log(allergies);
-    const store = formData.store;
-    console.log("preferred store:", store);
+      console.log(allergies);
+      const store = formData.store;
+      console.log("preferred store:", store);
 
-    const prompt = shoppingListPrompt(allergies, store, ingredients, titles);
-    const groceryList = await getGroceryList(prompt);
-    /* grocery list looks like this if logged:
+      const prompt = shoppingListPrompt(allergies, store, ingredients, titles);
+      const groceryList = await getGroceryList(prompt);
+      setLoading(false);
+      /* grocery list looks like this if logged:
        {text: '\n\nAldi Shopping List:\n\nProduce:\n2 red bell peppers…e\nRice vinegar\nHoney\n\nBakery:\n4-6 flour tortillas', index: 0, logprobs: null, finish_reason: 'stop'}
 
       I would like the data reformatted so it can be rendered to the user as shown in return. 
     */
-    // setList(groceryList.text);
-    console.log(groceryList);
+      // setList(groceryList.text);
+      console.log(groceryList);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <ShoppingListContainer>
-      <GetShoppingList
-        onClick={() => {
-          handleGetShoppingList(listData);
-        }}
-      >
-        Get Shopping List
-      </GetShoppingList>
+      {error && (
+        <>
+          <p>
+            There was an error creating the shopping list. Please try again.
+          </p>
+          <GetShoppingList
+            onClick={() => {
+              handleGetShoppingList(listData);
+            }}
+          >
+            Get Shopping List
+          </GetShoppingList>
+        </>
+      )}
+      {loading ? (
+        <p>Loading Shopping List...</p>
+      ) : (
+        <GetShoppingList
+          onClick={() => {
+            handleGetShoppingList(listData);
+          }}
+        >
+          Get Shopping List
+        </GetShoppingList>
+      )}
+
       <div>
         {list}
         {/* 
