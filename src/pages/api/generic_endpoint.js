69ec92f582e6c8c7a6cf1f4a7816afec26a7cd7a
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-05-24 15:34:55 -0400

    reverse reverse

--- src/pages/api/generic_endpoint.js
+++ src/pages/api/generic_endpoint.js
@@ -14,7 +14,7 @@ const handler = async (req, res) => {
       const { prompt } = req.body;
 
       const response = await openai.createCompletion({
-        model: "gpt-3.5-turbo",
+        model: "text-davinci-003",
         prompt: prompt,
         temperature: 0,
         max_tokens: 1000,
