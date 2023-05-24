8f9e738039c9ed06eacb119f725e94863c919eb4
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-05-24 15:23:24 -0400

    changed model

--- src/pages/api/fetch_meals.js
+++ src/pages/api/fetch_meals.js
@@ -17,7 +17,7 @@ const handler = async (req, res) => {
       console.log("prompt:", prompt);
 
       const response = await openai.createCompletion({
-        model: "text-davinci-003",
+        model: "gpt-3.5-turbo",
         prompt: prompt,
         temperature: 0,
         max_tokens: 1000,
--- src/pages/api/generic_endpoint.js
+++ src/pages/api/generic_endpoint.js
@@ -14,7 +14,7 @@ const handler = async (req, res) => {
       const { prompt } = req.body;
 
       const response = await openai.createCompletion({
-        model: "text-davinci-003",
+        model: "gpt-3.5-turbo",
         prompt: prompt,
         temperature: 0,
         max_tokens: 1000,
